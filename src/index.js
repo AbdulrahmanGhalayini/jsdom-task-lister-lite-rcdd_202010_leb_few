85d8607c4becea5c61a424e4c20f1f787e570e9a
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-12-10 14:12:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.getElementById("create-task-form").addEventListener("submit", function(event){
   event.preventDefault()
 let newt=document.getElementById("new-task-description").value;
-let t=document.getElementById("task")
+let t=document.getElementById("task").in
   
   
   
